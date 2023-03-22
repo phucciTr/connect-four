45c0e403de7435860f7489b56be4060b5846b280
phucciTr <miabam26@gmail.com>
2023-03-21 21:39:32 -0700

    Syntax adjustment

--- client/components/Cell.jsx
+++ client/components/Cell.jsx
@@ -6,9 +6,9 @@ const Cell = (props) => {
   let player = props.board[props.row] ?
                 props.board[props.row][props.col] : null;
 
-  return !player ?
-    <EmptyCell id={`${props.row}${props.col}`} col={props.col} handleClick={props.handleClick} handleHover={props.handleHover} currentTurn={props.currentTurn} isOnHoverCol={props.isOnHoverCol} /> :
-    <ColoredCell id={`${props.row}${props.col}`}  col={props.col} handleClick={props.handleClick} handleHover={props.handleHover} player={player} />;
+  return !player
+    ? <EmptyCell id={`${props.row}${props.col}`} col={props.col} handleClick={props.handleClick} handleHover={props.handleHover} currentTurn={props.currentTurn} isOnHoverCol={props.isOnHoverCol} />
+    : <ColoredCell id={`${props.row}${props.col}`}  col={props.col} handleClick={props.handleClick} handleHover={props.handleHover} player={player} />;
 }
 
 export default Cell;
